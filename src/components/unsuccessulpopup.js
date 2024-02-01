44c6a4edaabaf9ccf257fa733bcb3b4066804c48
Chigoziri123 <anuna.s@bullseyemanpower.com>
2024-02-01 07:26:38 +0100

    Unsuccessful page added

---
+++ src/components/unsuccessulpopup.js
@@ -0,0 +1,38 @@
+import React from "react";
+import notokLogo from "../Assets/notOKLogo.png"; 
+import backgroundImg from "../Assets/backgroundImg.png";
+
+const unsuccessfulPopUp = ({ onClose, onProceed }) => {
+  return (
+    <div className="fixed top-0 left-0 w-full h-full flex items-center min-h-screen p-4 justify-center bg-opacity-75 bg-cover" style={{ backgroundImage: `url(${backgroundImg})` }}>
+      <div className="bg-gray-700 p-8 rounded-lg w-full md:w-96">
+        {/* Close Button */}
+        <button className="text-gray-600 text-4xl font-thin absolute border border-solid border-red-600 hover:bg-red-600 top-2 right-2" onClick={onClose}>
+          X
+        </button>
+
+        {/* OK Logo */}
+        <div className="flex justify-center mb-6">
+          <img src={notokLogo} alt="OK Logo" className="w-16 h-16" />
+        </div>
+
+        {/* Success Message */}
+        <p className="text-center text-4xl font-semibold text-red-200 mb-6">
+          Registration was unsuccessful.
+        </p>
+
+        {/* Tertiary Button */}
+        <button
+          className="bg-green-700 text-white py-2 px-4 text-2xl rounded-full w-full mb-4 hover:bg-green-600"
+          onClick={onProceed}
+        >
+          Return to Dashboard
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default unsuccessfulPopUp;
+
+
