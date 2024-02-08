7d378a0e0c4776be820e641ab013fae94a9ea83f
izygabs <80975143+izygabs@users.noreply.github.com>
2024-02-08 13:24:27 +0100

    email reset validation

--- src/EntryPoint/EmailReset.js
+++ src/EntryPoint/EmailReset.js
@@ -1,6 +1,20 @@
 import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 function EmailReset() {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email("Invalid email").required("Email is required"),
+    }),
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   return (
     <div className="w-[25rem] h-[26rem] bg-[white] shadow-2xl flex flex-col justify-around items-center absolute py-5 rounded-[10px] backdrop-blur-3xl backdrop-brightness-150">
       <h1 className="text-2xl text-primary font-semibold font-asset">
@@ -8,13 +22,22 @@ function EmailReset() {
       </h1>
       <div className="flex flex-col justify-between gap-8 items-center">
         <p className="text-xl text-[#565872]">Enter your registered email</p>
-        <form className=" w-80 h-20 flex flex-col justify-between gap-8">
-          <input
-            className="h-12 px-3 py-2 bg-white border-2 border-[#0c7c3f] shadow-sm placeholder-slate-400 focus:outline-none placeholder:italic placeholder:text-base placeholder:text-[#565872]  focus:border-[#0c7c3f] focus:ring-[#0c7c3f] block w-80 rounded sm:text-sm focus:ring-1 max-sm:w-60 max-sm:ml-10"
-            name="email"
-            type="email"
-            placeholder="Email"
-          />
+        <form
+          onSubmit={formik.handleSubmit}
+          className=" w-80 h-20 flex flex-col justify-between gap-8"
+        >
+          <div>
+            <input
+              className="h-12 px-3 py-2 bg-white border-2 border-[#0c7c3f] shadow-sm placeholder-slate-400 focus:outline-none placeholder:italic placeholder:text-base placeholder:text-[#565872]  focus:border-[#0c7c3f] focus:ring-[#0c7c3f] block w-80 rounded sm:text-sm focus:ring-1 max-sm:w-60 max-sm:ml-10"
+              name="email"
+              type="email"
+              placeholder="Email"
+              {...formik.getFieldProps("email")}
+            />
+            {formik.touched.email && formik.errors.email ? (
+              <div className="text-xs text-[red]">{formik.errors.email}</div>
+            ) : null}
+          </div>
         </form>
       </div>
       <div className="pb-8">
