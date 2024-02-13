674a14a445867fdb35aee617fcd953c7a3e9ae35
Chigoziri123 <anuna.s@bullseyemanpower.com>
2024-02-13 22:32:45 +0100

    Added LGAs and States

--- src/components/StepperControl.js
+++ src/components/StepperControl.js
@@ -1,29 +1,34 @@
-import React from 'react'
+import React from 'react';
 
-const StepperControl = ({handleClick, currentStep, steps}) => {
+const StepperControl = ({ handleClick, handleSubmit, currentStep, steps }) => {
+  const handleNextButtonClick = () => {
+    handleSubmit(); // Call the handleSubmit function
+    handleClick("next"); // Call the handleClick function with "next" argument
+  };
+
   return (
     <div className='container flex justify-around m-1'> 
+      {/* Back button */}
+      <button 
+        onClick={handleClick}
+        className={`bg-[#ffffff] text-[#718096] uppercase py-1 px-2 rounded-xl font-semibold 
+        cursor-pointer border-2 border-[#8993a4] hover:bg-[#3e4e62] hover:text-[#ffffff] transition 
+        duration-200 ease-in-out ${currentStep === 1 ? 'opacity-50 cursor-not-allowed':''}`}
+      >
+        Back
+      </button>
 
-    {/* Back button */}
-  <button 
-    onClick={() => handleClick()}
-    className={`bg-[#ffffff] text-[#718096] uppercase py-1 px-2 rounded-xl font-semibold 
-    cursor-pointer border-2 border-[#8993a4] hover:bg-[#3e4e62] hover:text-[#ffffff] transition 
-    duration-200 ease-in-out ${currentStep === 1 ? 'opacity-50 cursor-not-allowed':''}`}
-    >Back</button>
-
-    {/*Next button */}
-    <button 
-    onClick={() => handleClick("next")}
-    className='bg-[#4CAF50] text-[#ffffff] uppercase py-1 px-2 rounded-xl font-semibold 
-    cursor-pointer hover:bg-[#3e4e62] hover:text-[#ffffff] transition 
-    duration-200 ease-in-out'>
-      {currentStep === steps.length -1 ? 'Confirm': 'Next'}
-    </button>
-
-
+      {/* Next button */}
+      <button 
+        onClick={handleNextButtonClick} // Call the new function on button click
+        className='bg-[#4CAF50] text-[#ffffff] uppercase py-1 px-2 rounded-xl font-semibold 
+        cursor-pointer hover:bg-[#3e4e62] hover:text-[#ffffff] transition 
+        duration-200 ease-in-out'
+      >
+        {currentStep === steps.length -1 ? 'Confirm': 'Next'}
+      </button>
     </div>
-  )
+  );
 }
 
-export default StepperControl
+export default StepperControl;
--- src/components/steps/StepOne.js
+++ src/components/steps/StepOne.js
@@ -2,109 +2,84 @@ import { useState, useContext } from 'react';
 import { StepperContext } from '../../contexts/StepperContext';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import StatesAndLGAs from "./necessarydata.json";
 
 export default function StepOne() {
-  const { userData, setUserData } = useContext(StepperContext);
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setUserData({ ...userData, [name]: value });
-  };
+  const { userData, setUserData, setCurrentStep, totalSteps } = useContext(StepperContext);
 
   const [fullName, setFullName] = useState("");
   const [dob, setDob] = useState(null);
   const [selectedGender, setSelectedGender] = useState("");
+  const [genderOptions] = useState(["Male", "Female"]);
 
-  // State variables for field errors
+  // Error states
   const [fullNameError, setFullNameError] = useState("");
   const [dobError, setDobError] = useState("");
   const [genderError, setGenderError] = useState("");
-
-  // Dropdown Variables
-  const [isGenderDropdownOpen, setIsGenderDropdownOpen] = useState(false);
-
-  // Dropdown options
-  const genderOptions = ["Male", "Female"];
+  const [addressError, setAddressError] = useState("");
+  const [stateError, setStateError] = useState("");
+  const [lgaError, setLgaError] = useState("");
 
   const handleBlur = (field, value) => {
-    if (!value.trim()) {
-      switch (field) {
-        case "fullName":
-          setFullNameError("*This is a required field.");
-          break;
-        case "dob":
-          setDobError("*This is a required field.");
-          break;
-        case "gender":
-          setGenderError("*This is a required field.");
-          break;
-        default:
-          break;
-      }
-    } else {
-      switch (field) {
-        case "fullName":
-          setFullNameError("");
-          break;
-        case "dob":
-          setDobError("");
-          break;
-        case "gender":
-          setGenderError("");
-          break;
-        default:
-          break;
-      }
+    switch (field) {
+      case "fullName":
+        setFullNameError(value.trim() ? "" : "*This is a required field.");
+        break;
+      case "dob":
+        setDobError(value ? "" : "*This is a required field.");
+        break;
+      case "gender":
+        setGenderError(value ? "" : "*This is a required field.");
+        break;
+      case "address":
+        setAddressError(value.trim() ? "" : "*This is a required field.");
+        break;
+      case "state":
+        setStateError(value ? "" : "*This is a required field.");
+        break;
+      case "lga":
+        setLgaError(value ? "" : "*This is a required field.");
+        break;
+      default:
+        break;
     }
   };
 
-  const handleGenderSelect = (gender) => {
-    setSelectedGender(gender);
-    setIsGenderDropdownOpen(false);
-  };
-
-  const handleDobSelect = (value) => {
-    setDob(value);
-  };
-
-  // Handle form submission for this section
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    // Validation logic for full name
+    // Check if all fields are filled
     if (!fullName.trim()) {
       setFullNameError("*This is a required field.");
       return;
-    } else {
-      setFullNameError("");
     }
-
-    // Validation logic for date of birth
     if (!dob) {
       setDobError("*This is a required field.");
       return;
-    } else {
-      setDobError("");
     }
-
-    // Validation logic for gender
     if (!selectedGender) {
       setGenderError("*This is a required field.");
       return;
-    } else {
-      setGenderError("");
     }
-
-    // If all validations pass, proceed to the next section
-    // onNext();
+    if (!userData.address.trim()) {
+      setAddressError("*This is a required field.");
+      return;
+    }
+    if (!userData.state) {
+      setStateError("*This is a required field.");
+      return;
+    }
+    if (!userData.lga) {
+      setLgaError("*This is a required field.");
+      return;
+    }
+    // Proceed to the next step if all fields are filled
+    setCurrentStep((prevStep) => prevStep + 1);
   };
 
   return (
     <div className='flex flex-col'>
       <div className="container mx-auto px-4 lg:px-0 bg-white">
-        {/* Registration Form */}
         <div className="max-w-2xl mx-auto ">
-          {/* Full Name Section */}
           <div className="text-black font-semibold text-lg max-md:max-w-full max-md:mt-2 max-sm:text-xs max-sm:mt-1">
             Full Name (as it appears in your documents)
             <input
@@ -115,47 +90,38 @@ export default function StepOne() {
               onBlur={() => handleBlur("fullName", fullName)}
               placeholder="Enter your full name"
             />
-            {fullNameError && <p className="text-red-500 sm:text-xs mt-1">{fullNameError}</p>}
+            {fullNameError && <p className="text-[#ef4444] sm:text-xs mt-1">{fullNameError}</p>}
           </div>
 
-
-          {/* Date of Birth (DOB) & Gender Sections */}
           <div className="mt-2 max-sm:mt-1 max-md:max-w-full ">
             <div className="gap-4 flex max-md:flex-col max-md:items-stretch max-md:gap-0 ">
               <div className="items-stretch grow flex flex-col max-md:max-w-full lg:w-1/2 max-md:mt-2 max-sm:mt-1">
                 <div className="text-black text-lg font-semibold max-md:max-w-full  max-sm:text-xs">Date of Birth</div>
-                <div className="items-stretch border flex justify-between h-[32px] gap-0 mt-2 px-6 rounded-xl border-solid border-green-700 max-md:max-w-full max-md:flex-wrap max-md:px-5">
-                  <div className="max-sm:pt-2 text-black text-lg grow max-sm:text-xs">{}
-                    <DatePicker selected={dob} onChange={handleDobSelect} placeholderText="MM/DD/YYYY" />
+                <div className="items-stretch border flex justify-between h-[32px] focus:outline-none gap-0 mt-2 px-6 rounded-xl border-solid border-green-700 max-md:max-w-full max-md:flex-wrap max-md:px-5">
+                  <div className="max-sm:pt-2 text-black focus:outline-none text-lg grow max-sm:text-xs">{}
+                    <DatePicker selected={dob} onChange={setDob} placeholderText="MM/DD/YYYY" onBlur={() => handleBlur("dob", dob)} />
                   </div>
                 </div>
-                {dobError && <p className="text-red-500 mt-1">{dobError}</p>}
+                {dobError && <p className="text-[#ef4444] mt-1">{dobError}</p>}
               </div>
 
               <div className="items-stretch grow flex flex-col max-md:max-w-full lg:w-1/2 max-sm:mt-1 max-md:mt-2">
                 <div className="text-black text-lg font-semibold max-md:max-w-full max-sm:text-xs">Gender</div>
-                <div
-                  className="items-stretch border flex justify-between gap-0 mt-2 px-6 rounded-xl h-[32px] border-solid border-green-700 max-md:max-w-full max-md:flex-wrap max-md:px-5"
-                  onClick={() => setIsGenderDropdownOpen(!isGenderDropdownOpen)}>
-                  <div className="max-sm:pt-2 text-black text-lg grow  max-sm:text-xs">{selectedGender || "Specify"}</div>
-                  <img
-                    loading="lazy"
-                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/7ce047be0e562e64bf8a16c54af587005b102e240974d41a4f1bfbdf3284720b?"
-                    className="aspect-[2] object-contain object-center w-[22px] fill-black shrink-0 my-auto  "
-                    onClick={() => setIsGenderDropdownOpen(!isGenderDropdownOpen)}
-                  />
-                </div>
-                {isGenderDropdownOpen && (
-                  <div className="flex flex-col mt-2 max-sm:mt-1 border border-solid border-green-700 rounded-md">
-                    {genderOptions.map((option) => (
-                      <div key={option} className="p-2 cursor-pointer hover:bg-gray-200 max-sm:text-xs" onClick={() => handleGenderSelect(option)}>
-                        {option}
-                      </div>
-                    ))}
+                  <div className='relative'>
+                    <select
+                      className="mt-2 px-6 h-[32px] border-solid w-full border border-green-700 rounded-md"
+                      value={selectedGender}
+                      onChange={(e) => setSelectedGender(e.target.value)}
+                      onBlur={() => handleBlur("gender", selectedGender)}
+                    >
+                      <option value="" disabled>Select</option>
+                      {genderOptions.map((option) => (
+                        <option key={option} value={option}>{option}</option>
+                      ))}
+                    </select>
                   </div>
-                )}
-                {genderError && <p className="text-red-500 mt-1 max-sm:text-xs">{genderError}</p>}
-              </div>
+                  {genderError && <p className="text-[#ef4444] mt-1 max-sm:text-xs">{genderError}</p>}
+                </div>
             </div>
           </div>
 
@@ -164,42 +130,51 @@ export default function StepOne() {
             Contact Address
           </div>
           <div className="items-stretch flex justify-between gap-5 mt-2 max-sm:mt-1 max-md:max-w-full max-md:flex-wrap">
-            {/* Apartment Field */}
-            <input className="text-black text-lg justify-center max-sm:text-xs items-stretch border grow px-6 h-[32px] rounded-xl border-solid border-green-700 max-md:max-w-full max-md:px-5"
+            <input
+              className={`text-black text-lg justify-center max-sm:text-xs items-stretch border grow px-6 h-[32px] rounded-xl border-solid ${addressError ? 'border-red-500' : 'border-green-700'} max-md:max-w-full max-md:px-5`}
               placeholder="Home Address"
-              type="text">
-            </input>
+              type="text"
+              value={userData.address}
+              onChange={(e) => setUserData({ ...userData, address: e.target.value })}
+              onBlur={() => handleBlur("address", userData.address)}
+            />
+            {addressError && <p className="text-[#ef4444] sm:text-xs mt-1">{addressError}</p>}
           </div>
 
           {/* LGA/State Field */}
           <div className="items-stretch max-sm:mt-1 flex justify-evenly gap-1.5 max-md:max-w-full mt-2 max-md:flex-wrap">
-
-            <div className="flex items-stretch border max-sm:mt-1 justify-between max-sm:text-xs h-[32px] mt-2 px-6 w-full rounded-xl border-solid border-green-700 max-md:max-w-full max-md:flex-wrap lg:w-1/2 max-md:px-5">
-              <input
-                type="text"
-                placeholder="LGA"
-                className="text-black max-sm:text-xs text-lg grow">
-              </input>
-              <img
-                loading="lazy"
-                src="https://cdn.builder.io/api/v1/image/assets/TEMP/64462fc9619791bf8b68c2def239fa154ee59f0442c07837798cfb86fe3dd443?"
-                className="aspect-[2.22] object-contain object-center w-[22px] fill-black shrink-0 my-auto"
-              />
+            <div className={`flex items-stretch border max-sm:mt-1 justify-between max-sm:text-xs h-[32px] mt-2 px-6 w-full rounded-xl border-solid ${stateError ? 'border-red-500' : 'border-green-700'} max-md:max-w-full max-md:flex-wrap lg:w-1/2 max-md:px-5`}>
+              <select
+                value={userData.state}
+                onChange={(e) => setUserData({ ...userData, state: e.target.value })}
+                name='state'
+                className='text-black max-sm:text-xs text-lg grow'>
+                  <option value=''>Select State</option>
+                  {StatesAndLGAs.map((state) => (
+                    <option key={state.state} value={state.state}>{state.state}</option>
+                  ))}
+                </select>
             </div>
 
-            <div className="flex items-stretch border max-sm:mt-1 justify-between max-sm:text-xs h-[32px] mt-2 px-6 w-full rounded-xl border-solid border-green-700 max-md:max-w-full max-md:flex-wrap lg:w-1/2 max-md:px-5">
-              <input
-                type="text"
-                placeholder="State"
-                className="text-black max-sm:text-xs text-lg grow"></input>
-              <img
-                loading="lazy"
-                src="https://cdn.builder.io/api/v1/image/assets/TEMP/64462fc9619791bf8b68c2def239fa154ee59f0442c07837798cfb86fe3dd443?"
-                className="aspect-[2.22] object-contain object-center w-[22px] fill-black shrink-0 my-auto"
-              />
+            <div className={`flex items-stretch border max-sm:mt-1 justify-between max-sm:text-xs h-[32px] mt-2 px-6 w-full rounded-xl border-solid ${lgaError ? 'border-red-500' : 'border-green-700'} max-md:max-w-full max-md:flex-wrap lg:w-1/2 max-md:px-5`}>
+              <select
+                value={userData.lga}
+                onChange={(e) => setUserData({ ...userData, lga: e.target.value })}
+                name='lga'
+                className='text-black max-sm:text-xs text-lg grow'>
+                  <option value=''>Select LGA</option>
+                  {userData.state &&
+                  StatesAndLGAs.find((state) => state.state === userData.state)?.lgas.map((lga) => (
+                    <option key={lga} value={lga}>{lga}</option>
+                  ))}
+                </select>
             </div>
           </div>
         </div>
+      </div>
+      {/* Navigation buttons */}
+      <div className="flex justify-end mt-6 mr-8">
+        <button className="text-white bg-indigo-500 hover:bg-indigo-600 px-6 py-2 rounded-lg" onClick={handleSubmit}>Next</button>
       </div>
     </div>
   );
