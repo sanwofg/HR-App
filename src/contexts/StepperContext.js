6597a343614b58aac9e7a6b71989b86119edd57f
Samuel~ <chigozirianuna@gmail.com>
2024-02-25 13:05:04 +0100

    Reworking registration form

--- src/contexts/StepperContext.js
+++ src/contexts/StepperContext.js
@@ -4,16 +4,16 @@
 
 
 // StepperContext.js
-import React, { createContext, useState } from 'react';
+// import React, { createContext, useState } from 'react';
 
-export const StepperContext = createContext();
+// export const StepperContext = createContext();
 
-export const StepperProvider = ({ children }) => {
-  const [userData, setUserData] = useState(null); // Initialize with null or default value
+// export const StepperProvider = ({ children }) => {
+//   const [userData, setUserData] = useState(null); // Initialize with null or default value
 
-  return (
-    <StepperContext.Provider value={{ userData, setUserData }}>
-      {children}
-    </StepperContext.Provider>
-  );
-};
+//   return (
+//     <StepperContext.Provider value={{ userData, setUserData }}>
+//       {children}
+//     </StepperContext.Provider>
+//   );
+// };
