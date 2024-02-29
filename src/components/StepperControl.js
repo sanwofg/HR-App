f261b8a5043cc47ec145ab3b948b24aa968d206b
Samuel~ <chigozirianuna@gmail.com>
2024-02-29 16:52:58 +0100

    Adjusted back button

--- src/components/StepperControl.js
+++ src/components/StepperControl.js
@@ -1,10 +1,11 @@
 const StepperControl = ({ handleSubmit, currentStep, steps }) => {
-  
+  const isBackDisabled = currentStep === 1;
   return (
     <div className="container flex justify-around m-1">
       {/* Back button */}
       <button
         onClick={() => handleSubmit("back")}
+        disabled={isBackDisabled}
         className={`bg-[#ffffff] text-[#718096] uppercase py-2 px-8 rounded-xl font-thin 
     cursor-pointer border-2 border-[#8993a4] hover:bg-[#3e4e62] hover:text-[#ffffff] transition 
     duration-200 ease-in-out ${
